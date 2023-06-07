f34d6a0cbd51f63555425e7f3877fb1c673acd12
Prakash L <121917226+prakashlohanathan@users.noreply.github.com>
2023-06-07 16:36:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import Sidebar from "./component/Sidebar";
+import Dashboard from "./component/Dashboard";
+import AddUser from "./component/AddUser"
+import Profile from './component/Profile'
+import PendingRequest from './component/PendingRequest'
+import Task from "./component/Task";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+import ProfileDetails from "./component/ProfileDetails";
+import ResetPassword from "./component/ResetPassword";
+import { useState } from "react";
+function App() {
+  let [users,setUsers] = useState([
+    {
+      name:"Anoop",
+      email:"anoop@gmail.com",
+      mobile:"919100201",
+      dob:"27/1/1993"
+    },
+    {
+      name:"Maha",
+      email:"maha@gmail.com",
+      mobile:"8919989291",
+      dob:"20/3/1998"
+    },
+    {
+      name:"Ganesh",
+      email:"ganesh@gmail.com",
+      mobile:"80989019801",
+      dob:"12/12/2000"
+    },
+    {
+      name:"Sharma",
+      email:"sharma@gmail.com",
+      mobile:"782718189",
+      dob:"9/9/2001"
+    }
+  ])
+  return <>
+  <BrowserRouter>
+    <div id="wrapper">
+        <Sidebar/>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard users={users} setUsers={setUsers}/>}/>
+          <Route path="/add-user" element={<AddUser users={users} setUsers={setUsers}/>}/>
+          <Route path="/edit-user/:id" element={<AddUser users={users} setUsers={setUsers}/>}/>
+          <Route path="/profile" element={<Profile/>}>
+              <Route path='details' element={<ProfileDetails/>}/>
+              <Route path='reset-password' element={<ResetPassword/>}/>
+          </Route>
+          <Route path="/pending-request" element={<PendingRequest/>}/>
+          <Route path="/task" element={<Task/>}/>
+          <Route path="*" element={<Navigate to='/dashboard'/>}/>
+        </Routes>
+    </div>
+  </BrowserRouter>
+  </>
+}
+
+export default App;
---
+++ src/component/AddUser.js
@@ -0,0 +1,80 @@
+import React, { useState,useEffect } from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useNavigate, useParams } from 'react-router-dom';
+
+function AddUser({users,setUsers}) {
+
+  let navigate = useNavigate()
+  let params = useParams()
+
+  let [name,setName] = useState("")
+  let [email,setEmail] = useState("")
+  let [mobile,setMobile] = useState("")
+  let [dob,setDob] = useState("")
+
+  let handleSubmit = ()=>{
+    let newUser = { name,email,mobile,dob }
+    let newArray = [...users]
+    if(params.id!==undefined)
+    {
+      newArray.splice(params.id,1,newUser)
+    }
+    else
+    {
+      newArray.push(newUser)
+    }
+    setUsers(newArray)
+    navigate('/dashboard')
+  }
+
+  // useEffect(()=>{
+  //   console.log("Use Effect Triggered")
+  // })//Triggered when a state changes
+
+  // useEffect(()=>{
+  //   console.log("Use Effect Triggered")
+  // },[])//Triggered only for the first time when the component is loading
+
+  // useEffect(()=>{
+  //   console.log("Use Effect Triggered")
+  // },[email,dob]) //Triggered when the mentioned state changes
+
+  useEffect(()=>{
+    if(params.id !== undefined)
+    {
+        setName(users[params.id].name)
+        setEmail(users[params.id].email)
+        setMobile(users[params.id].mobile)
+        setDob(users[params.id].dob)
+    }
+  },[params.id,users])//dependancy array added to remove the warning. But not mandatory
+
+  return <>
+    <div className='container-fluid'>
+    <Form>
+    <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter name" value={name} onChange={(e)=>setName(e.target.value)}/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Mobile</Form.Label>
+        <Form.Control type="text" placeholder="Enter mobile" value={mobile} onChange={(e)=>setMobile(e.target.value)}/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Date Of Birth</Form.Label>
+        <Form.Control type="date" value={dob} onChange={(e)=>setDob(e.target.value)}/>
+      </Form.Group>
+      <Button variant="primary" onClick={()=>handleSubmit()}>
+        Submit
+      </Button>
+    </Form>
+    </div>
+  </>
+}
+
+export default AddUser
